e6367e929007c3a10624ad6beba9561ca51ea592
Leonid Shevtsov <leonid@shevtsov.me>
2012-06-19 13:17:30 +0300

    refactoring

--- monkey_sort.js
+++ monkey_sort.js
@@ -1,88 +1,105 @@
+// The comparison matrix
+function ComparisonMatrix(items) {
+  var self = this;
+  self.items = items;
+  self.matrix = {};
+    
+  _.each(self.items, function(item) {
+    self.matrix[item] = {};
+    self.matrix[item][item] = '=';
+  });
+
+  self.opposite = function(value) {
+    return value=='=' ? '=' : (value == '<' ? '>' : '<');
+  };
+  
+  self.get = function(a,b) {
+    if (self.matrix[a][b]) {
+      return self.matrix[a][b];
+    } else {
+      throw {items: [a,b]};
+    }
+  };
+
+  self.set = function(a, b, value) {
+    self.updateSingle(a, b, value);
+    self.updateSingle(b, a, self.opposite(value));
+  };
+
+  self.updateSingle = function(a, b, value) {
+    self.matrix[a][b] = value;
+    self.updateTransitive(a, b);
+  };
+  
+  self.updateTransitive = function(a, b) {
+    if (self.matrix[a][b] == '=') {
+      // ((Cij = “=”) ⋀ (Cjk is known)) ⇒ Cik = Cjk
+      _.each(_.keys(self.matrix[b]), function(c) {
+        if (!matrix[a][c]) {
+          self.updateSingle(a, c, self.matrix[b][c]);
+        }
+      });
+    } else {
+      // (Cij ∈ { “<”, “>”}) ⋀ (Cjk ∈ {Cij, “=”}) ⇒ Cik = Cij
+      _.each(_.keys(self.matrix[b]), function(c) {
+        if (!self.matrix[a][c] && ((self.matrix[a][b] == self.matrix[b][c]) || (self.matrix[b][c] == '='))) {
+          self.updateSingle(a, c, self.matrix[a][b]);
+        }
+      });
+    }
+  };
+}
+  
+// This is the very simplest form of quick sort.
+// Unknown comparison interrupt is done inside the matrix.get() method
+function quickSort(items, matrix) {
+  var array = items;
+  function qsortPart(low, high) {
+    var i = low;
+    var j = high;
+    var x = array[Math.floor((low+high)/2)];
+    do {
+      while  (matrix.get(array[i], x) == '>') i++;
+      while  (matrix.get(array[j], x) == '<') j--;
+      if (i<=j) {
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        i++;
+        j--;
+      }
+    } while (i<=j);
+
+    if (low < j) {
+      qsortPart(low, j);
+    }
+    if (i < high) {
+      qsortPart(i, high);
+    }
+  }
+
+  qsortPart(0, array.length-1);
+}
+
 $(function() {
-  var matrix = {};
-  var items = [];
-
-  function recordComparison(a, b, value) {
-    updateSingle(a, b, value);
-    updateSingle(b, a, -value);
-    console.log(matrix);
-  }
-
-  function updateSingle(a, b, value) {
-    matrix[a][b] = value;
-    updateTransitive(a, b);
-  }
-
-  function updateTransitive(a, b) {
-    _.each(_.keys(matrix[b]), function(c) {
-      if ((matrix[a][b] == matrix[b][c]) || (matrix[b][c] === 0)) {
-        // TODO need more transitiveness here
-        matrix[a][c] = matrix[a][b];
-      }
-    });
-  }
-
-  function initMatrix() {
-    matrix = {};
-    _.each(items, function(item) {
-      matrix[item] = {};
-      matrix[item][item] = 0;
-    });
-  }
-
-  function quickSort() {
-    var array = items;
-    function qsortPart(low, high) {
-      var i = low;
-      var j = high;
-      var x = array[Math.floor((low+high)/2)];
-      do {
-        while (true) {
-          if (typeof(matrix[array[i]][x])=='undefined') {
-            askUser(array[i], x);
-            return false;
-          } else if (matrix[array[i]][x] == -1) {
-            ++i;
-          } else break;
-        }
-        while (true) {
-          if (typeof(matrix[x][array[j]])=='undefined') {
-            askUser(x, array[j]);
-            return false;
-          } else if (matrix[x][array[j]] == -1) {
-            --j;
-          } else break;
-        }
-        if (i<=j) {
-          var temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
-          i++;
-          j--;
-        }
-      } while (i<=j);
-
-      if (low < j) {
-        if (!qsortPart(low, j)) return false;
-      }
-      if (i < high) {
-        if (!qsortPart(i, high)) return false;
-      }
-      return true;
-    }
-
-    if (qsortPart(0, array.length-1)) {
-      showResults();
-    }
-  }
+  var matrix;
+  var items;
 
   $('#submit').click(function(e) {
     e.preventDefault();
     items = _(_(_($('#items').val().split("\n")).map(function(s){return s.trim();})).reject(function(s){return s==="";})).uniq();
-    
-    initMatrix();
-    quickSort();
+    matrix = new ComparisonMatrix(items);
+    tryQuickSort();
   });
+
+  function tryQuickSort() {
+    try {
+      quickSort(items, matrix);
+      showResults();
+    } catch(e) {
+      askUser(e.items[0], e.items[1]);
+    }
+  }
 
   function askUser(a, b) {
     $('#ask').show();
@@ -94,9 +111,9 @@ $(function() {
     e.preventDefault();
     var a = $('#ask_a').text();
     var b = $('#ask_b').text();
-    var result = Number($(this).data('result'));
-    recordComparison(a,b,result);
-    quickSort();
+    var result =$(this).data('result');
+    matrix.set(a,b,result);
+    tryQuickSort();
   });
 
   function showResults() {
